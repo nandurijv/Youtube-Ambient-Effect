6a91f091b4c940c92308b9185d523135ebf14dd4
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2023-03-26 10:59:08 +0530

    improved readability

--- main.py
+++ main.py
@@ -11,28 +11,27 @@ def ambient_matrix_info(vid_name):
     #show info about the video
     meta = get_meta_data(vid_path)
     print(meta)
+
+    #clear the output
+    file_to_delete = open("OutputCss/style.css",'w')
+    file_to_delete.close()
+
+    #write initial values to the output
     write_to_file(f'#video{{\n\tanimation: a1 {int(meta[0]/(meta[1]/2))}s linear forwards;\n}}\n')
     write_to_file(f'@keyframes a1{{\n\t')
+
     # import all the frames into the Frames directory
     read_frames(vid_path)
 
-    box_shadow_matrix = []
     # read the chroma subsampling values in a matrix
     for i in range(0,int(meta[0]),int(meta[1])):
-        box_shadow_matrix.append(get_pixel_matrix(f'{img_path}Frame{i}',i,meta[0]))
+        get_pixel_matrix(f'{img_path}Frame{i}',i,meta[0])
+    
+    # format the css
     with open('OutputCss/style.css', 'rb+') as fh:
         fh.seek(-1, 2)
         fh.truncate()
     write_to_file(f';}}\n')
-    
-    # find the most significant color
-
-
-    # make transition matrix
-
-
-    # return the matrix as output with a movie file.
-
 
 #call to the main function
 ambient_matrix_info(vid_path)
